90ebdb29f6ae79d45f72993656aa69c1a29a1db7
Chitransh-j <chitranshjawere3@gmail.com>
2024-06-04 14:19:20 +0530

    added prisma client

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -3,14 +3,14 @@ import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 import PatContextProvider from "@/context/pat-context-provider";
 import SearchContextProvider from "@/context/search-context-provider";
+import prisma from "@/lib/db";
 import { Pat } from "@/lib/types";
 
 export default async function layout( {children} : {children : React.ReactNode}) {
-  const res  = await fetch('https://bytegrad.com/course-assets/projects/petsoft/api/pets') 
-  if (!res.ok){
-    throw new Error('Could not fetch data')
-  }
-  const data: Pat[] = await res.json()
+  
+  const data = await prisma.pat.findMany({
+    //where clause
+  })
 
   return (
     <>
